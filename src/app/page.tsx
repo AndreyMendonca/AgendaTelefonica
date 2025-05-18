bed1f08316ac699ebe3fe4593f1a7b4f195a2a48
Andrey Mendonça <97072306+AndreyMendonca@users.noreply.github.com>
2025-05-18 16:41:53 -0300

    Telas criadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,93 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { useEnderecoService } from "@/resources/Endereco.service";
+import { Endereco } from "@/types/Endereco";
+import { useState } from "react";
+
+const Page = () => {
+  const [cep, setCep] = useState("");
+  const service = useEnderecoService();
+  const [endereco, setEndereco] = useState<Endereco | null>(null)
+
+  const handlePesquisarCep = async () => {
+    try {
+      if (!(!/^\d{8}$/.test(cep))) {
+        const response = await service.buscas(cep);
+        setEndereco(response);
+      } else {
+        alert('CEP em formato invalido')
+      }
+    } catch (error) {
+      alert('Cep invalido ou não encontrado')
+    }
+  }
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <>
+      <div className="bg-indigo-600 min-h-screen flex flex-col items-center">
+        <div className="flex flex-col gap-2 bg-white p-10 rounded-2xl w-full max-w-xl">
+          <div className="flex items-center gap-2 w-full">
+            <label className="w-22">CEP: </label>
+            <input type="text"
+              name={cep}
+              onChange={e => setCep(e.target.value)}
+              placeholder="CEP"
+              className="bg-indigo-50 border border-indigo-400 rounded-md p-2 w-full focus:ring-1 focus:ring-indigo-300 focus:outline-none"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="flex items-center gap-2 w-full">
+            <label className="w-22">Endereço: </label>
+            <input type="text"
+              placeholder="Digite o Endereço"
+              className="bg-indigo-50 border border-indigo-400 rounded-md p-2 w-full focus:ring-1 focus:ring-indigo-300 focus:outline-none"
+            />
+          </div>
+          <div className="flex items-center gap-2 w-full">
+            <div className="flex items-center flex-3">
+              <label className="w-26">Bairro: </label>
+              <input type="text"
+                placeholder="Digite o Bairro"
+                className="bg-indigo-50 border border-indigo-400 rounded-md p-2 w-full focus:ring-1 focus:ring-indigo-300 focus:outline-none"
+              />
+            </div>
+            <div className="flex items-center flex-1">
+              <label className="pr-2">Nº: </label>
+              <input type="text"
+                placeholder="Número"
+                className="bg-indigo-50 border border-indigo-400 rounded-md p-2 w-full focus:ring-1 focus:ring-indigo-300 focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className="flex items-center gap-2 w-full">
+            <div className="flex items-center flex-1">
+              <label className="w-40">UF: </label>
+              <input type="text"
+                placeholder="Selecione a UF"
+                className="bg-indigo-50 border border-indigo-400 rounded-md p-2 w-full focus:ring-1 focus:ring-indigo-300 focus:outline-none"
+              />
+            </div>
+            <div className="flex items-center flex-3">
+              <label className="pr-2">Cidade </label>
+              <input type="text"
+                placeholder="Número"
+                className="bg-indigo-50 border border-indigo-400 rounded-md p-2 w-full focus:ring-1 focus:ring-indigo-300 focus:outline-none"
+              />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <button onClick={handlePesquisarCep}>Buscar</button>
+      </div>
+
+
+      
+      <p>
+        {
+          endereco !== null ? `Endereco ${endereco?.logradouro} - ${endereco?.bairro} - ${endereco?.localidade}/${endereco?.uf}` : ''
+        }
+      </p>
+    </>
+  )
 }
+
+export default Page;
---
+++ src/resources/Endereco.service.ts
@@ -0,0 +1,18 @@
+import { Endereco } from "@/types/Endereco";
+
+class EnderecoService {
+    baseURL: string = "https://viacep.com.br/ws/"
+
+    async buscas(cep: string): Promise<Endereco>{
+        const url = this.baseURL + `${cep}/json/`;
+        const response = await fetch(url);
+
+        if(response.status === 400){
+            throw new Error()
+        }
+        const endereco:Endereco = await response.json();
+        return endereco;
+    }
+}
+
+export const useEnderecoService = () => new EnderecoService();
